24ed8a20b512f62f9991bc5bafc3ebdccf4c08e7
Sidney <caiosidney69@gmail.com>
2021-12-24 15:39:24 -0300

    WENDEL PAGA O LANCHE SEGUNDA :)

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const port = 8080
+const path = require('path')
+const bodyparser = require('body-parser')
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/album',(req,res)=>{
+    res.render('album.html')
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`API  IS RUNING IN PORT ${port}`)
+})
